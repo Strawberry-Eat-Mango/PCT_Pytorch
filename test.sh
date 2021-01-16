c4cca4fbbe922da5d89785761a962cf3469b7f55
Yi_Zhang <zhangyi970819@gmail.com>
2021-01-16 23:25:40 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python main.py --exp_name=test --num_points=1024 --k=20 --use_sgd=True --eval=True --model_path=checkpoints/best/models/model.t7 --test_batch_size 8
+python main.py --exp_name=test --num_points=1024 --use_sgd=True --eval=True --model_path=checkpoints/best/models/model.t7 --test_batch_size 8
