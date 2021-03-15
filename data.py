3b5fc7643a30e586b3127951fc7ceefe9cf76c4d
pompeii222 <50950127+pompeii222@users.noreply.github.com>
2021-03-15 21:17:03 +0800

    Update data.py

--- data.py
+++ data.py
@@ -4,25 +4,25 @@ import h5py
 import numpy as np
 from torch.utils.data import Dataset
 
-def download():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
-    if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-        www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
-        zipfile = os.path.basename(www)
-        os.system('wget %s; unzip %s' % (www, zipfile))
-        os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
-        os.system('rm %s' % (zipfile))
+# def download():
+#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#     DATA_DIR = os.path.join(BASE_DIR, 'data')
+#     if not os.path.exists(DATA_DIR):
+#         os.mkdir(DATA_DIR)
+#     if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
+#         www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+#         zipfile = os.path.basename(www)
+#         os.system('wget %s; unzip %s' % (www, zipfile))
+#         os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
+#         os.system('rm %s' % (zipfile))
 
 def load_data(partition):
-    download()
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = os.path.join(BASE_DIR, 'data')
+#     download()
+#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#     DATA_DIR = os.path.join(BASE_DIR, 'data')
     all_data = []
     all_label = []
-    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
+    for h5_name in glob.glob(os.path.join('/kaggle/input', 'modelnet40_ply_hdf5_2048', 'ply_data_%s*.h5'%partition)):
         f = h5py.File(h5_name)
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
